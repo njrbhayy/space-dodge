94256443f6d7a2f0155151bf9540cf730e2d6159
Nirbhay <njg.43767@gmail.com>
2024-02-06 11:39:03 +0530

    Added more features

--- main.py
+++ main.py
@@ -1,34 +1,65 @@
 import pygame
 import random
 import time
+pygame.font.init()
 
 WIDTH, HEIGHT = 1000, 700
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Space Dodge')
 
 BG = pygame.transform.scale(pygame.image.load('bg.jpeg'), (WIDTH, HEIGHT))
+FONT = pygame.font.SysFont('Consolas', 30)
 
 PLAYER_WIDTH = 40
 PLAYER_HEIGHT = 60
-
 PLAYER_VEL = 5
 
-def draw(object):
+PROJECTILE_WIDTH = 10
+PROJECTILE_HEIGHT = 20
+PROJECTILE_VEL = 3
+
+
+def draw(player, elapsed_time, projectiles):
     WINDOW.blit(BG, (0, 0))
 
-    pygame.draw.rect(WINDOW, "red", object)
+    time_text = FONT.render(f"Time: {round(elapsed_time)}s", 1, 'white')
+    WINDOW.blit(time_text, (20, 20))
+
+    pygame.draw.rect(WINDOW, "red", player)
+
+    for projectile in projectiles:
+        pygame.draw.rect(WINDOW, 'white', projectile)
 
     pygame.display.update()
 
 def main():
     running = True
 
-    clock = pygame.time.Clock()
-    
     player = pygame.Rect(200, HEIGHT - PLAYER_HEIGHT, PLAYER_WIDTH, PLAYER_HEIGHT)
+    
+    clock = pygame.time.Clock()
+
+    start_time = time.time()
+    elapsed_time = 0
+
+    projectiles_add_increment = 2000
+    projectiles_count = 0
+
+    projectiles = []
+    hit = False
 
     while running:
-        clock.tick(60)
+        projectiles_count += clock.tick(60)
+        elapsed_time = time.time() - start_time
+
+        if projectiles_count > projectiles_add_increment:
+            for _ in range(3):
+                projectile_x = random.randint(0, WIDTH - PROJECTILE_WIDTH)
+                projectile = pygame.Rect(projectile_x, -PROJECTILE_HEIGHT, PROJECTILE_WIDTH, PROJECTILE_HEIGHT)
+                projectiles.append(projectile)
+
+            projectiles_add_increment = max(200, projectiles_add_increment - 50)
+            projectiles_count = 0
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -46,7 +77,23 @@ def main():
         if keys[pygame.K_RIGHT] and player.x + PLAYER_VEL + PLAYER_WIDTH <= WIDTH:
             player.x += PLAYER_VEL
 
-        draw(player)
+        for projectile in projectiles[:]:
+            projectile.y += PROJECTILE_VEL
+            if projectile.y > HEIGHT:
+                projectiles.remove(projectile)
+            elif projectile.y >= player.y and projectile.colliderect(player):
+                projectiles.remove(projectile)
+                hit = True
+                break
+
+        if hit:
+            lost_text = FONT.render("You Lost!", 1, "white")
+            WINDOW.blit(lost_text, (WIDTH/2 - lost_text.get_width()/2, HEIGHT/2 - lost_text.get_height()/2))
+            pygame.display.update()
+            pygame.time.delay(4000)
+            break
+
+        draw(player, elapsed_time, projectiles)
 
     pygame.quit()
 
